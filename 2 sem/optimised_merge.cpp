b4b13bbf557d3d41265a968398e6ccc7f554f60b
furs-aka-beast <kiruha200409@gmail.com>
2023-02-16 15:07:56 +0300

    done mergesort measuring and modification task

--- 2 sem/optimised_merge.cpp
+++ 2 sem/optimised_merge.cpp
@@ -9,23 +9,28 @@
 using i64 = int64_t;
 using Array = std::vector<i64>;
 
-void InsertionSort(Array::iterator begin, Array::iterator end){
+void InsertionSort(Array::iterator begin, Array::iterator end)
+{
     i64 n = end - begin;
     Array::iterator it = begin;
-    for (i64 i = 0; i < n; i++){
-	it = begin + i;
-	i64 x = *it;
-	while (it != begin){
-            if (x < *it){
-	        *(it+1) = *it;
-	        *it = x;
-	    }
-	    it--;
-	}
-	if (x < *it){
-	   *(it+1) = *it;
-	   *it = x;
-	}
+    for (i64 i = 0; i < n; i++)
+    {
+        it = begin + i;
+        i64 x = *it;
+        while (it != begin)
+        {
+            if (x < *it)
+            {
+                *(it + 1) = *it;
+                *it = x;
+            }
+            it--;
+        }
+        if (x < *it)
+        {
+            *(it + 1) = *it;
+            *it = x;
+        }
     }
 }
 
@@ -80,7 +85,7 @@ void Merge(Array::iterator a_beg, Array::iterator a_end, Array::iterator b_beg, 
     }
     return;
 }
-void MergeSort(Array::iterator beg, size_t n, Array::iterator buff)
+void MergeSort(Array::iterator beg, size_t n, Array::iterator buff, i64 act)
 {
     if (n == 1)
         return;
@@ -89,12 +94,19 @@ void MergeSort(Array::iterator beg, size_t n, Array::iterator buff)
         if (*beg > *(beg + 1))
             std::swap(*beg, *(beg + 1));
     }
-    MergeSort(beg, n / 2, buff);
-    MergeSort(beg + n / 2, n - n / 2, buff);
-    Merge(beg, beg + n / 2, beg + n / 2, beg + n, buff);
-    for (i64 i = 0; i < n; i++)
+    else if (n < act)
     {
-        *(beg + i) = *(buff + i);
+        InsertionSort(beg, beg + n);
+    }
+    else
+    {
+        MergeSort(beg, n / 2, buff, act);
+        MergeSort(beg + n / 2, n - n / 2, buff, act);
+        Merge(beg, beg + n / 2, beg + n / 2, beg + n, buff);
+        for (i64 i = 0; i < n; i++)
+        {
+            *(beg + i) = *(buff + i);
+        }
     }
 }
 void FillArray(Array::iterator begin, i64 n)
@@ -111,23 +123,30 @@ void FillArray(Array::iterator begin, i64 n)
 int main()
 {
     std::ofstream fout;
-    fout.open("no_optimise.csv");
-    fout<<"number of elements"<<","<<"time1"<<","<<"time2"<<","<<"time3"<<","<<"time4"<<std::endl;
-    for (i64 n = 500; n <= 100000; n+=500)
+    fout.open("optimised.csv");
+    fout << "number of elements"
+         << ","
+         << "time1"
+         << ","
+         << "time2"
+         << ","
+         << "time3"
+         << ","
+         << "time4" << std::endl;
+    for (i64 n = 500; n <= 100000; n += 500)
     {
-        fout<<n;
-        for(i64 i=0;i<4;i++)
+        fout << n;
+        for (i64 i = 0; i < 4; i++)
         {
-        Array data(n);
-        FillArray(data.begin(), n);
-        Array buff(data.size());
-        auto start = std::chrono::steady_clock::now();
-        MergeSort(data.begin(), data.size(), buff.begin());
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end-start;
-        fout<<","<<(double)elapsed_seconds.count();
+            Array data(n);
+            FillArray(data.begin(), n);
+            Array buff(data.size());
+            auto start = std::chrono::steady_clock::now();
+            MergeSort(data.begin(), data.size(), buff.begin(),16);
+            auto end = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            fout << "," << (double)elapsed_seconds.count();
         }
-        fout<<std::endl;
-
+        fout << std::endl;
     }
 }
