ce1bfc8bf488960da71efdfa0a1c32cea3c202ed
furs-aka-beast <kiruha200409@gmail.com>
2023-04-20 16:40:15 +0300

    done unique_ptr

--- 2 sem/Pointers/unique_ptr.h
+++ 2 sem/Pointers/unique_ptr.h
@@ -12,10 +12,23 @@ template <class T> class SmartPtr{
     }
     SmartPtr(const SmartPtr&) = delete;
     SmartPtr& operator= (const SmartPtr&) = delete;
+    SmartPtr& operator= (SmartPtr&& p) {
+        if(this != &p){
+            Move(p);
+        }
+        return *this;
+    };
+    SmartPtr(SmartPtr&& p){
+        Move(p);
+    }
     ~SmartPtr(){
         delete(ptr);
     }
     private:
     T * ptr;
+    void Move(SmartPtr &p){
+        ptr=p.ptr;
+        p.ptr=nullptr;
+    }
 
 };
