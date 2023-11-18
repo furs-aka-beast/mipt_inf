1992a291f27bd9cc538f63365a030cced240d377
furs-aka-beast <kiruha200409@gmail.com>
2023-11-18 13:21:55 +0300

    починил

--- 3_sem/10th_week/for_each_task_1.cpp
+++ 3_sem/10th_week/for_each_task_1.cpp
@@ -6,7 +6,7 @@ int main(){
     std::cin>>s;
     auto l=[shift=3](char &ch){
         if(ch>'Z') ch-='a'-'A';
-        ch+=shift;
+        ch+=shift%('Z'-'A'+1);
         if(ch>'Z') ch-='Z'-'A'+1;
     };
     std::for_each(s.begin(), s.end(),l);
